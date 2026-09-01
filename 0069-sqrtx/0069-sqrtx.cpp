f7b33c828dd9066baeaeddfb460ce1eb437a5440
Priyanshu Kumar <kumarpriyanshuu08@gmail.com>
2026-09-01 15:07:00 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.13%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x < 2)
+            return x;
+        long long st = 1;
+        long long end = x;
+        long long ans = 0;
+        while (st <= end) {
+            long long mid = st + (end - st) / 2;
+            if (mid * mid == x) {
+                return mid;
+            }
+            else if (mid * mid < x) {
+                ans = mid;
+                st = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
